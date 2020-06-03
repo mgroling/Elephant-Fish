eda2da4f275e8d695bb779742e5d0f865081ec52
Gabriel Kressin <gabriel2000x@hotmail.de>
2020-06-03 23:23:14 +0200

    added analysis functions from Moritz

--- src/analysis.py
+++ src/analysis.py
@@ -1,1 +1,60 @@
 # File to analyse the results
+# Copied from Moritz Maxeiner Masterthesis: https://git.imp.fu-berlin.de/bioroboticslab/robofish/ai
+
+from numpy import float32
+from numpy.linalg import norm
+import reader
+
+
+def normalize_series(x):
+    """
+    Given a series of vectors, return a series of normalized vectors.
+    Null vectors are mapped to `NaN` vectors.
+    """
+    return (x.T / norm(x, axis=-1)).T
+
+
+def calc_iid(a, b):
+    """
+    Given two series of poses - with X and Y coordinates of their positions as the first two elements -
+    return the inter-individual distance (between the positions).
+    """
+    return norm(b[:, :2] - a[:, :2], axis=-1)
+
+
+def calc_tlvc(a, b, tau_min, tau_max):
+    """
+    Given two velocity series and both minimum and maximum time lag return the
+    time lagged velocity correlation from the first to the second series.
+    """
+    length = tau_max - tau_min
+    return float32(
+        [
+            (a[t] @ b[t + tau_min :][:length].T).mean()
+            for t in range(min(len(a), len(b) - tau_max + 1))
+        ]
+    )
+
+
+def calc_follow(a, b):
+    """
+    input: a,b 2d array with x and y values
+    output: follow metric as defined by Moritz Maxeiner
+    """
+    a_v = a[1:, :2] - a[:-1, :2]
+    b_p = normalize_series(b[:-1, :2] - a[:-1, :2])
+    print(b_p)
+    return (a_v * b_p).sum(axis=-1)
+
+
+def main():
+    file = "data/sleap_1_Diffgroup1-1.h5"
+    fish1 = reader.extract_coordinates(file, [b'center'], [0])
+    fish2 = reader.extract_coordinates(file, [b'center'], [1])
+
+    follow = calc_follow(fish1, fish2)
+    print(follow)
+
+
+if __name__ == "__main__":
+    main()
