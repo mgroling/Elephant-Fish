1553cefabb996a0a850c87e9aac77e18e061230e
marc131183 <marcg99@zedat.fu-berlin.de>
2020-06-04 12:20:44 +0200

    integrated execution into files

--- src/locomotion.py
+++ src/locomotion.py
@@ -3,6 +3,7 @@ import pandas as pd
 import math
 from functions import getAngle, getDistance
 from itertools import chain
+from reader import *
 
 def getLocomotion(np_array, path_to_save_to):
     """
@@ -40,3 +41,19 @@ def getLocomotion(np_array, path_to_save_to):
 
     df = pd.DataFrame(data = output[1:], columns = output[0])
     df.to_csv(path_to_save_to, index = None, sep = ";")
+
+def main():
+    file = "data/sleap_1_Diffgroup1-1.h5"
+
+    temp = extract_coordinates(file, [b'head', b'center'], fish_to_extract=[0,1,2])
+
+    #remove rows with Nans in it
+    temp = temp[~np.isnan(temp).any(axis=1)]
+
+    print("shape:",temp.shape)
+
+    #get locomotion and save it
+    getLocomotion(temp , "data/locomotion_data.csv")
+
+if __name__ == "__main__":
+    main()
