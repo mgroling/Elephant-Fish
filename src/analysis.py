e8f0eed795b88453e3b518e96512520f5735c0e5
marc131183 <marcg99@zedat.fu-berlin.de>
2020-07-11 14:33:37 +0200

    added plot function for getClusters that shows count elems for each cluster

--- src/analysis.py
+++ src/analysis.py
@@ -4,6 +4,8 @@
 from sklearn.cluster import KMeans
 import pandas as pd
 import numpy as np
+import collections
+import matplotlib.pyplot as plt
 
 from functions import *
 
@@ -47,11 +49,10 @@ def calc_follow(a, b):
     return (a_v * b_p).sum(axis=-1)
 
 
-def getClusters(paths, path_to_save, count_clusters = (20, 20, 20)):
+def getClusters(paths, path_to_save, count_clusters = (20, 20, 20), verbose = False):
     """
     paths should be iterable
     path_to_save is the folder in which it will be saved
-    round will round to one decimal place (round = 2 -> all values will be of the form x.yz)
     Finds cluster centers for the dataframes (for mov, pos, ori) (count_clusters should be a tuple with (count_clusters_mov, count_clusters_pos, count_clusters_ori)) and saves these clusters
     """
     #right now it thinks that 0 and 2*pi are not the same for pos and ori, maybe find a solution for it
@@ -69,9 +70,64 @@ def getClusters(paths, path_to_save, count_clusters = (20, 20, 20)):
     df_pos = df_all[ang_pos_cols].melt(var_name = "columns", value_name = "value")
     df_ori = df_all[ang_ori_cols].melt(var_name = "columns", value_name = "value")
 
+    df_pos["value"] = convertRadiansRange(df_pos["value"].to_numpy())
+    df_ori["value"] = convertRadiansRange(df_ori["value"].to_numpy())
+
     kmeans_mov = KMeans(n_clusters = count_clusters[0]).fit(df_mov["value"].to_numpy().reshape(-1, 1))
     kmeans_pos = KMeans(n_clusters = count_clusters[1]).fit(df_pos["value"].to_numpy().reshape(-1, 1))
     kmeans_ori = KMeans(n_clusters = count_clusters[2]).fit(df_ori["value"].to_numpy().reshape(-1, 1))
+
+    if verbose == True:
+        freq_mov = collections.Counter(kmeans_mov.labels_)
+        centers = kmeans_mov.cluster_centers_
+        x, y = zip(*freq_mov.items())
+        x, y = list(x), list(y)
+        for i in range(0, len(x)):
+            x[i] = float(centers[x[i]])
+        temp_1 = [X for _,X in sorted(zip(x,y))]
+        temp_2 = [Y for Y,_ in sorted(zip(x,y))]
+        plt.figure(figsize = (24, 16))
+        ax = plt.bar(np.arange(count_clusters[0]), temp_1)
+        plt.title("Elements per cluster for linear movement", fontsize = 30)
+        plt.xlabel("clusters", fontsize = 30)
+        plt.ylabel("count elements", fontsize = 30)
+        plt.xticks(np.arange(count_clusters[0]), np.round(temp_2, 3))
+        plt.savefig("figures/mov_elems_per_cluster_" + str(count_clusters[0]))
+        plt.clf()
+
+        freq_pos = collections.Counter(kmeans_pos.labels_)
+        centers = kmeans_pos.cluster_centers_
+        x, y = zip(*freq_pos.items())
+        x, y = list(x), list(y)
+        for i in range(0, len(x)):
+            x[i] = float(centers[x[i]])
+        temp_1 = [X for _,X in sorted(zip(x,y))]
+        temp_2 = [Y for Y,_ in sorted(zip(x,y))]
+        plt.figure(figsize = (24, 16))
+        ax = plt.bar(np.arange(count_clusters[0]), temp_1)
+        plt.title("Elements per cluster for change in position (radians)", fontsize = 30)
+        plt.xlabel("clusters", fontsize = 30)
+        plt.ylabel("count elements", fontsize = 30)
+        plt.xticks(np.arange(count_clusters[0]), np.round(temp_2, 3))
+        plt.savefig("figures/pos_elems_per_cluster_" + str(count_clusters[0]))
+        plt.clf()
+        
+        freq_ori = collections.Counter(kmeans_ori.labels_)
+        centers = kmeans_ori.cluster_centers_
+        x, y = zip(*freq_ori.items())
+        x, y = list(x), list(y)
+        for i in range(0, len(x)):
+            x[i] = float(centers[x[i]])
+        temp_1 = [X for _,X in sorted(zip(x,y))]
+        temp_2 = [Y for Y,_ in sorted(zip(x,y))]
+        plt.figure(figsize = (24, 16))
+        ax = plt.bar(np.arange(count_clusters[0]), temp_1)
+        plt.title("Elements per cluster for change in orientation (radians)", fontsize = 30)
+        plt.xlabel("clusters", fontsize = 30)
+        plt.ylabel("count elements", fontsize = 30)
+        plt.xticks(np.arange(count_clusters[0]), np.round(temp_2, 3))
+        plt.savefig("figures/ori_elems_per_cluster_" + str(count_clusters[0]))
+        plt.clf()
 
     with open(path_to_save + "clusters.txt", "w+") as f:
         f.write("count_clusters(mov, pos, ori)\n" + str(count_clusters) + "\n")
@@ -81,3 +137,7 @@ def getClusters(paths, path_to_save, count_clusters = (20, 20, 20)):
             f.write(str(float(elem)) +"\n")
         for elem in kmeans_ori.cluster_centers_:
             f.write(str(float(elem)) +"\n")
+
+if __name__ == "__main__":
+    paths = ["data/locomotion_data_diff1.csv", "data/locomotion_data_diff2.csv", "data/locomotion_data_diff3.csv", "data/locomotion_data_diff4.csv"]
+    getClusters(paths, "data/", (20,20,20), verbose = True)
