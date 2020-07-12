e8e67b03c68e38907d33da97238e22ce093e6c44
Gabriel Kressin <gabriel2000x@hotmail.de>
2020-07-12 17:28:57 +0200

    finalized convPolarToCart

--- src/functions.py
+++ src/functions.py
@@ -203,8 +203,36 @@ def convPolarToCart( polarTracks, distances ):
         [
             [center1_x, center1_y, orientation1, ...]
             [center1_x, center1_y, orientation1, ...]
+            ...
         ]
     distances:
         [disCH1, disCH2, ...]
+    Output:
+        [
+            [head1_x, head1_y, center1_x, center1_y, ...]
+            [head1_x, head1_y, center1_x, center1_y, ...]
+            ...
+        ]
     """
     nfish = len( distances )
+    rows, cols = polarTracks.shape
+    assert cols % nfish == 0
+    assert cols // nfish == 3
+    assert rows >= 1
+
+    out = np.empty( (rows, nfish * 4) )
+
+    # Indices
+    isp_xcenter = [ 3 * x for x in range(nfish) ]
+    isp_ycenter = [ 3 * x + 1 for x in range(nfish) ]
+    iso_xcenter = [ 4 * x + 2 for x in range(nfish) ]
+    iso_ycenter = [ 4 * x + 3 for x in range(nfish) ]
+
+    out[:,iso_xcenter] = polarTracks[:,isp_xcenter]
+    out[:,iso_ycenter] = polarTracks[:,isp_ycenter]
+
+    for f in range(nfish):
+        out[:, 4 * f] = out[:, 4 * f + 2] + np.cos( polarTracks[:, 3 * f + 2] ) * distances[f]
+        out[:, 4 * f + 1] = out[:, 4 * f + 3] + np.sin( polarTracks[:, 3 * f + 2] ) * distances[f]
+
+    return out
