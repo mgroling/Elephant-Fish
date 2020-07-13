a37ca8f30f03305b8e3bc4c70cccf74f35e60347
Gabriel Kressin <gabriel2000x@hotmail.de>
2020-07-13 15:36:19 +0200

    added function to visualize tracks on empty tank

--- src/visualization.py
+++ src/visualization.py
@@ -21,9 +21,12 @@ diff2_out = 'C:/Users/Gabriel/Videos/diff_2_tracks.avi'
 
 # OpenCV Version 4.3.0.36
 
-def addTracksOnVideo( inputvideo, outputvideo, tracks, nfish = 3, fps=30, dimension=(960,720), psize=1, showvid=False ):
+def addTracksOnVideo( inputvideo, outputvideo, tracks, nfish = 3, fps=30, dimension=(960,720), psize=1, showvid=False, skeleton=None ):
     """
     Takes tracks and adds them on video
+    skeleton is a mapping indices for each point in tracks
+    Fishskeleton: [(0,1), (0,2), (0,3), (1,2), (1,3), (2,4), (3,5), (2,6), (3,7), (6,8), (7,8), (8,9)]
+    Only Center and Head: [(0,1)]
     """
     row, col = tracks.shape
     assert row >= 1
@@ -55,10 +58,17 @@ def addTracksOnVideo( inputvideo, outputvideo, tracks, nfish = 3, fps=30, dimens
         if success:
             # frame = cv2.flip( frame, 0 )
             for f in range(nfish):
+                points = []
                 for n in range(nnodes//2):
                     x = int( tracks[i,f * nnodes + 2 * n] )
                     y = int( tracks[i,f * nnodes + 2 * n + 1] )
-                    frame = cv2.circle( frame, ( x, y ), psize, colors[f], -1 )
+                    points.append( ( x, y, ) )
+                for p in points:
+                    frame = cv2.circle( frame, p, psize, colors[f], -1 )
+                # Lines between points
+                if skeleton is not None:
+                    for p1,p2 in skeleton:
+                        frame = cv2.line( frame, points[p1], points[p2], colors[f], 1 )
             out.write( frame )
             if showvid:
                 cv2.imshow( 'fishy fish fish', frame )
@@ -78,11 +88,70 @@ def addTracksOnVideo( inputvideo, outputvideo, tracks, nfish = 3, fps=30, dimens
         print( "Not the entire trackset was used" )
 
 
+def addTracksOnTank( outputvideo, tracks, tank="data/tank.png", nfish = 3, fps=30, dimension=(960,720), psize=1, showvid=False, skeleton=None ):
+    """
+    Takes tracks and adds them on video
+    skeleton is a mapping indices for each point in tracks
+    Fishskeleton: [(0,1), (0,2), (0,3), (1,2), (1,3), (2,4), (3,5), (2,6), (3,7), (6,8), (7,8), (8,9)]
+    Only Center and Head: [(0,1)]
+    """
+    row, col = tracks.shape
+    assert row >= 1
+    assert col > 0
+    assert col % nfish == 0
+    nnodes = col // nfish
+
+    # Set up input
+    img = cv2.imread( tank, 1 )
+    if img is None:
+        print( "not able to open tank" )
+        sys.exit(-1)
+
+    # Set up output
+    fourcc = cv2.VideoWriter_fourcc( 'D', 'I', 'V', 'X' )
+    out = cv2.VideoWriter( outputvideo, fourcc, fps, dimension )
+
+    # Process video
+    colors = [(0,255,255), (0,255,0), (0,0,255)]
+    i = 0
+    while i < row:
+        if i % 1000 == 0:
+            print( "Frame: ", i )
+
+        frame = img.copy()
+        for f in range(nfish):
+            points = []
+            for n in range(nnodes//2):
+                x = int( tracks[i,f * nnodes + 2 * n] )
+                y = int( tracks[i,f * nnodes + 2 * n + 1] )
+                points.append( ( x, y, ) )
+            for p in points:
+                frame = cv2.circle( frame, p, psize, colors[f], -1 )
+            # Lines between points
+            if skeleton is not None:
+                for p1,p2 in skeleton:
+                    frame = cv2.line( frame, points[p1], points[p2], colors[f], 1 )
+        out.write( frame )
+        if showvid:
+            cv2.imshow( 'fishy fish fish', frame )
+            cv2.waitKey(0)
+        else:
+            print( "end of video" )
+            break
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        i += 1
+
+    cv2.destroyAllWindows()
+
+
 def main():
 
-    tracks = reader.extract_coordinates( "data/sleap_1_diff2.h5", [b'head', b'center', b'l_fin_basis', b'r_fin_basis', b'l_fin_end', b'r_fin_end', b'l_body', b'r_body', b'tail_basis', b'tail_end'] )
+    tracks = reader.extract_coordinates( "data/sleap_1_diff2.h5", [b'head', b'center', b'l_fin_basis', b'r_fin_basis', b'l_fin_end', b'r_fin_end', b'l_body', b'r_body', b'tail_basis', b'tail_end'] )[0:17000]
 
-    addTracksOnVideo( diff2, diff2_out, tracks )
+    # addTracksOnVideo( diff2, diff2_out, tracks, showvid=True, skeleton=[(0,1), (0,2), (0,3), (1,2), (1,3), (2,4), (3,5), (2,6), (3,7), (6,8), (7,8), (8,9)] )
+    addTracksOnTank( diff2_out, tracks, showvid=True, skeleton=[(0,1), (0,2), (0,3), (1,2), (1,3), (2,4), (3,5), (2,6), (3,7), (6,8), (7,8), (8,9)] )
 
 
 
