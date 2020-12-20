5feaed110d3ffcbbe873c168102991da80ef4fb5
Gabriel Kressin <gabriel.kressin@fu-berlin.de>
2020-12-20 01:37:02 +0100

    rename filename to path, lazyLoad addition

--- src/io.py
+++ src/io.py
@@ -1,9 +1,26 @@
 # File to load track coordinates
 
 import numpy as np
+import pandas as pd
 
-def loadTrack( filename ):
+def loadTrackData( path ):
     """
-    Loads trackdata from filename
+    Loads trackData from path
     """
-    return np.load( filename )
+    return np.load( path )
+
+
+def lazytrackData( n ):
+    """
+    Loads predefined datasets
+    """
+    track1 = "data/trackData/track1.npy"
+    track2 = "data/trackData/track2.npy"
+    track3 = "data/trackData/track3.npy"
+    track4 = "data/trackData/track4.npy"
+    track5 = "data/trackData/track5.npy"
+    track6 = "data/trackData/track6.npy"
+    track7 = "data/trackData/track7.npy"
+    track8 = "data/trackData/track8.npy"
+    tracks = [ track1, track2, track3, track4, track5, track6, track7, track8 ]
+    return np.load( tracks[n] )
